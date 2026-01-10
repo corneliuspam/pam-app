348079180751fa2b3515dee7884917d37ff237c6
corneliuspam <corneliuspam8736@gmail.com>
2026-01-10 21:35:58 +0100

    Add files via upload

---
+++ models/Massage.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  from: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  to: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  text: String,
+  image: String, // image URL
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Message", messageSchema);
