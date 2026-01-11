061d94198a7e6ebd7cb22bc720b9f85aa0286e2e
corneliuspam <corneliuspam8736@gmail.com>
2026-01-11 21:08:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,46 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const fileUpload = require("express-fileupload");
 const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.json());
-app.use(fileUpload());
 app.use(express.static("public"));
-app.use("/uploads", express.static("uploads"));
 
-app.get("/dashboard", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "dashboard.html"));
+/* ROOT */
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const usersOnline = {};
+/* ONLINE USERS */
+const onlineUsers = new Set();
 
-// Upload profile picture
-app.post("/upload", (req, res) => {
-  if (!req.files) return res.status(400).send("No file");
-
-  const file = req.files.avatar;
-  const name = Date.now() + "_" + file.name;
-  file.mv(path.join(__dirname, "uploads", name));
-
-  res.json({ image: "/uploads/" + name });
-});
-
-// Socket.io
 io.on("connection", (socket) => {
-  socket.on("join", (username) => {
-    usersOnline[username] = true;
-    io.emit("status", usersOnline);
-  });
 
-  socket.on("disconnect", () => {
-    for (let user in usersOnline) {
-      if (usersOnline[user]) delete usersOnline[user];
-    }
-    io.emit("status", usersOnline);
+  socket.on("join", (username) => {
+    socket.username = username;
+    onlineUsers.add(username);
+    io.emit("online", Array.from(onlineUsers));
   });
 
   socket.on("chatMessage", (data) => {
     io.emit("chatMessage", {
-      ...data,
-      time: new Date().toLocaleTimeString(),
+      user: data.user,
+      message: data.message,
+      time: new Date().toLocaleTimeString()
     });
   });
 
-  socket.on("reaction", (data) => {
-    io.emit("reaction", data);
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      onlineUsers.delete(socket.username);
+      io.emit("online", Array.from(onlineUsers));
+    }
   });
 });
 
-server.listen(process.env.PORT || 3000, () =>
-  console.log("PAM App running")
-);
+server.listen(process.env.PORT || 3000, () => {
+  console.log("PAM App running");
+});
