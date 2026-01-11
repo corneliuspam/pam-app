ab888300ff590c4546ab3173d2510e9188a59179
corneliuspam <corneliuspam8736@gmail.com>
2026-01-11 16:01:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
 const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// IMPORTANT: Render provides PORT
 const PORT = process.env.PORT || 10000;
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
@@ -19,16 +22,24 @@ app.get("/dashboard", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "dashboard.html"));
 });
 
+// In-memory users (NO DB)
 let users = {};
 
+// Socket.io
 io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
   socket.on("join", (user) => {
+    // user = { name, avatar }
     users[socket.id] = user;
-    io.emit("online-users", Object.values(users));
+
+    // Notify everyone
     io.emit("system", `${user.name} is online`);
+    io.emit("online-users", Object.values(users));
   });
 
   socket.on("message", (data) => {
+    // data = { name, avatar, text }
     io.emit("message", data);
   });
 
@@ -39,5 +50,11 @@ io.on("connection", (socket) => {
       delete users[socket.id];
       io.emit("online-users", Object.values(users));
     }
+    console.log("User disconnected:", socket.id);
   });
+});
+
+// Start server
+server.listen(PORT, () => {
+  console.log("✅ PAM APP running on port", PORT);
 });
