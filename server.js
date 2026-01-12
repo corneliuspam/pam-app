c94fff8a864cff3003c91fd7a73e1a34376f5bf4
corneliuspam <corneliuspam8736@gmail.com>
2026-01-12 09:44:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,23 +7,30 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // important for Render / cross-origin
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
 
+// Serve public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 app.get("/dashboard", (req, res) => res.sendFile(path.join(__dirname, "public", "dashboard.html")));
 
+// Socket.IO for chat
 io.on("connection", (socket) => {
   console.log("A user connected");
 
+  // Receive chat message
   socket.on("chat message", (data) => {
-    console.log("Message received:", data);
-    io.emit("chat message", data);
+    io.emit("chat message", data); // broadcast to all
   });
+
+  // Handle online/offline
+  socket.on("user connected", (username) => console.log(username + " online"));
+  socket.on("user disconnected", (username) => console.log(username + " offline"));
 
   socket.on("disconnect", () => console.log("A user disconnected"));
 });
