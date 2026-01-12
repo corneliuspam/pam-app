8a365d56aec941ed49de962a09f7cc7a5b7f55f8
corneliuspam <corneliuspam8736@gmail.com>
2026-01-12 09:28:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,40 +5,28 @@ const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // important for Render / cross-origin
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// ===== Serve dashboard and index =====
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 app.get("/dashboard", (req, res) => res.sendFile(path.join(__dirname, "public", "dashboard.html")));
 
-// ===== SOCKET.IO =====
 io.on("connection", (socket) => {
-
   console.log("A user connected");
 
-  // When a chat message is sent
   socket.on("chat message", (data) => {
     console.log("Message received:", data);
-    io.emit("chat message", data); // broadcast to all users
+    io.emit("chat message", data);
   });
 
-  socket.on("user connected", (username) => {
-    console.log(username + " is online");
-  });
-
-  socket.on("user disconnected", (username) => {
-    console.log(username + " disconnected");
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
+  socket.on("disconnect", () => console.log("A user disconnected"));
 });
 
-// ===== START SERVER =====
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
